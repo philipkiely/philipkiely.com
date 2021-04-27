1db36a85b96f8b0ed13659f49c95d8f2eefe2442
Philip Kiely <kielyphi@grinnell.edu>
2021-04-27 06:33:21 -0500

    launch day special

--- assets/js/cefip.js
+++ assets/js/cefip.js
@@ -22,6 +22,27 @@ function setDiscount() {
             full = parseInt(p.innerHTML.substring(1));
             p.innerHTML = "<s>$" + full + "</s> $" + (full - (parseInt(params.get("discount") / 100)));
         });
+    } else {
+        var links = document.getElementsByTagName("a")
+        Array.prototype.slice.call(links).forEach((l) => {
+            if ((l.href.includes("https://gum.co") || l.href.includes("https://gumroad.com/l/")) && !l.href.includes("free")) {
+                if (l.href.includes("?")) {
+                    var base = l.href.split("?")[0]
+                    var rem = "?" + l.href.split("?")[1] //Max 1 "?" in URL
+                } else {
+                    var base = l.href
+                    var rem = ""
+                }
+                if (!base.endsWith("/")) {
+                    base += "/"
+                }
+                l.href = base + "launch/" + rem
+            }
+        });
+        var prices = document.getElementsByClassName("price-text")
+        Array.prototype.slice.call(prices).forEach((p) => {
+            p.innerHTML = "<s>$48</s> $36 launch-day special";
+        });
     }
 }
 
